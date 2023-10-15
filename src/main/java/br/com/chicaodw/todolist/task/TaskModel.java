7773997343676b34dab1aceb9e3de80cc447ce9e
Chicão <fccs@cesar.school>
2023-10-15 09:05:47 -0300

    Atualização de codigo para publicação

---
+++ src/main/java/br/com/chicaodw/todolist/task/TaskModel.java
@@ -0,0 +1,42 @@
+package br.com.chicaodw.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+
+@Data // cria gettrs e settrs
+@Entity(name = "td_task") // Cria o nome da tabela
+public class TaskModel {
+
+    @Id // gera a chave primaria
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+
+    @Column(length = 50) // Defeine o tamanho maximo de caracteres no title 
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public void setTitle(String title) throws Exception{
+        if (title.length() > 50) {
+            throw new Exception("O campo title deve ter no máximo 50 caracteres");
+        }
+
+        this.title = title;
+    }
+}
